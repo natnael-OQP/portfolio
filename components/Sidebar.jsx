159da9a9f9e8e6d968d4f47e13449f20c487bc4e
papa-react <makiya112545@gmail.com>
2021-12-29 08:10:25 -1200

    sidebar component

---
+++ components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+const Sidebar = () => {
+    return (
+		<div className=" rounded-xl bg-slate-400 col-span-full lg:col-span-3 p-2 md:p-4   ">
+			<h1>hello world</h1>
+		</div>
+	);
+}
+
+export default Sidebar
