ac99e7d69ae499e230635642ea113a6bbc76f69a
papa-react <makiya112545@gmail.com>
2021-12-30 07:57:44 -1200

    layout component

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,15 @@
-import Sidebar from "./Sidebar"
-
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 const Layout = ({ children }) => {
 	return (
-		<div className=" grid grid-cols-12 w-full min-h-screen px-5 md:px-20 py-10   gap-5   ">
+		<div className=" grid w-full h-full grid-cols-12 gap-5 px-5 py-10 md:py-14 h md:px-20  ">
+			<Header/>
 			<Sidebar />
-			<main className="bg-slate-100 grid lg:col-span-9 col-span-full rounded-xl p-2 md:p-4 ">
+			<main className="grid p-2 bg-slate-100 lg:col-span-9 col-span-full rounded-xl md:p-4 ">
 				{children}
 			</main>
 		</div>
 	);
 };
 
-export default Layout
+export default Layout;
