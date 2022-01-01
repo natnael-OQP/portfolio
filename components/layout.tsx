f198d07ffc774061cafff14f2d75c2c473f55cd7
papa-react <makiya112545@gmail.com>
2022-01-01 08:03:50 -1200

    style layouts

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }:{children: React.ReactNode }) => {
 		<div className="grid w-full h-full grid-cols-12 gap-y-6 md:gap-x-6 py-12  px-[8vw] md:py-14 h md:px-16 sm:px-[20vw]">
 			<Header/>
 			<Sidebar />
-			<main className="flex flex-col px-2 py-0 overflow-hidden bg-[#172A45] sd bl lg:col-span-9 col-span-full rounded-xl md:px-4 relative   ">
+			<main className="flex flex-col px-2 py-0 overflow-hidden bg-[#0a192f] sd bl lg:col-span-9 col-span-full rounded-xl md:px-4 relative   ">
 				<Navbar/>
 				{children}
 			</main>
