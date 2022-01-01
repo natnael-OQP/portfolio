ba13545df6e8f0d26c1a53b4d030279d206b0a40
papa-react <makiya112545@gmail.com>
2022-01-01 08:02:37 -1200

    interface

---
+++ interface.ts
@@ -0,0 +1,24 @@
+import { IconType } from "react-icons";
+
+export interface IAbout {
+    name: string,
+    title1: string,
+    title2: string,
+    description: string,
+    buttonText: string,
+}
+export interface ISkill{
+    name: string,
+    Icon: IconType,
+    level?:string,
+}
+
+export interface IProject{
+    name:string,
+    description:string,
+    image_path:string,
+    deployed_url:string,
+    github_url:string,
+    category:string[],
+    key_techs:string[],
+}
