20aac8959513b0261419f950427e06d56a5694d1
papa-react <makiya112545@gmail.com>
2022-01-03 08:09:21 -1200

    change project to name

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     const {pathname} = useRouter();
     const [active, setActive] = useState<string>("About");
     useEffect(() => {
-        if (pathname === "projects") setActive("Projects");
+        if (pathname === "projects") setActive("Work");
         if (pathname === "resume") setActive("Resume");
         
     },[])
@@ -35,13 +35,13 @@ const Navbar = () => {
 				marginLeft:'-1rem',
 				marginRight:'-1rem',
 			}}
-            className="justify-between px-4 py-3 shadow-sm fx"
+            className="justify-between px-6 py-3 shadow-sm fx"
         >
             <h1 className="text-xl font-semibold tracking-wider border-b font-lg text-color1 border-color1" >{active}</h1>
             <div className="space-x-4 fx " >
                 <NavItem id='01' active={active} setActive={setActive} name={'About'} route={'/'}  />
                 <NavItem id='02' active={active} setActive={setActive} name={'Resume'} route={'resume'}  />
-                <NavItem id='03' active={active} setActive={setActive} name={'Projects'} route={'projects'}  />
+                <NavItem id='03' active={active} setActive={setActive} name={'Work'} route={'projects'}  />
             </div>
         </div>
     )
