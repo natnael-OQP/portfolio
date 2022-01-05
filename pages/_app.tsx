874f51af5cc18ec5202c90710a31c90151dcb72b
papa-react <makiya112545@gmail.com>
2022-01-04 23:52:32 -1200

    applay progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const progress = new ProgressBar({
+    size: 1,
+    color: "#64ffda",
+    className: "z-50",
+    delay: 100,
+  });
+
+  Router.events.on('routeChangeStart', () => progress.start());
+  Router.events.on('routeChangeComplete', () => progress.finish());
+
   return (
     <Layout>
       <Component {...pageProps} />
