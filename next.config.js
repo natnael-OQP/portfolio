2481bff419a432dd1e539f99046c72ff71f38651
papa-react <makiya112545@gmail.com>
2022-01-05 01:08:04 -1200

    image Optimization

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ module.exports = {
 	reactStrictMode: true,
 	images: {
 		domains: ["avatars.githubusercontent.com"],
+		formats: ["image/avif", "image/webp"],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+		minimumCacheTTL: 60,
 	},
 };
